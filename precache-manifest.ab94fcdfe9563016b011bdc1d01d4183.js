5ee0fccda22033c75121aec15ac398b2de2600b5
m-onofri <manuele.onofri@gmail.com>
2019-12-18 12:26:26 +0100

    Updates

---
+++ precache-manifest.ab94fcdfe9563016b011bdc1d01d4183.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb79d27bd3a0c6c82d06",
+    "url": "/random_quotes_generator/static/css/main.a7976ad3.chunk.css"
+  },
+  {
+    "revision": "fb79d27bd3a0c6c82d06",
+    "url": "/random_quotes_generator/static/js/main.fb79d27b.chunk.js"
+  },
+  {
+    "revision": "52c307eabbeca7f57fea",
+    "url": "/random_quotes_generator/static/js/1.52c307ea.chunk.js"
+  },
+  {
+    "revision": "59de8bcb7efa56702d40",
+    "url": "/random_quotes_generator/static/js/runtime~main.59de8bcb.js"
+  },
+  {
+    "revision": "a324ab017d09dd7d9b980ada10f0e777",
+    "url": "/random_quotes_generator/index.html"
+  }
+];
